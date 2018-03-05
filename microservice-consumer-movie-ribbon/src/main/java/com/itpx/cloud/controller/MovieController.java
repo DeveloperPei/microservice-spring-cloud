6f5932ef614c1081306770cf78bccddbe3cc466a
732846637@qq.com <732846637@qq.com>
2018-03-05 19:56:55 +0800

    添加ribbon模块，简单实现负载均衡
    1主要是在启动类添加@LoadBalanced实现ribbon的负载均衡；
    2将调用服务提供者的url改为虚拟ip
    3ereka里面的依赖已经包含了ribbon依赖，不需要额外添加

---
+++ microservice-consumer-movie-ribbon/src/main/java/com/itpx/cloud/controller/MovieController.java
@@ -0,0 +1,26 @@
+package com.itpx.cloud.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import com.itpx.cloud.entity.User;
+
+@RestController
+public class MovieController {
+	@Autowired
+	private RestTemplate restTemplate;
+/*	@Value("${userUrl}")  
+	private String userUrl;*/
+	
+	@GetMapping("/movie/{id}")
+	public User findById(@PathVariable Long id){
+		//return restTemplate.getForObject("http://localhost:7900/simple/"+id, User.class);
+		//vip virtual ip 虚拟ip  microservice-provider-user
+		return restTemplate.getForObject("http://microservice-provider-user/simple/"+id, User.class);
+		//return restTemplate.getForObject(userUrl+id, User.class);
+	}
+}
