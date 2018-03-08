393c577e945f66bc2a44e1fd203badd546d88a9f
732846637@qq.com <732846637@qq.com>
2018-03-08 15:49:29 +0800

    不使用eureka的情况下使用ribbon
    1 新添加模块 ，通过配置文件修改，来禁止使用eureka，指定服务提供者IP
    2 没有配置ip的服务提供者不会再提供服务

---
+++ microservice-spring-cloud-ribbon-without-eureka/src/main/java/com/itpx/cloud/controller/MovieController.java
@@ -0,0 +1,43 @@
+package com.itpx.cloud.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import com.itpx.cloud.entity.User;
+
+@RestController
+public class MovieController {
+	@Autowired
+	private RestTemplate restTemplate;
+	@Autowired
+	private LoadBalancerClient loadBalancerClient;
+/*	@Value("${userUrl}")  
+	private String userUrl;*/
+	
+	@GetMapping("/movie/{id}")
+	public User findById(@PathVariable Long id){
+		//return restTemplate.getForObject("http://localhost:7900/simple/"+id, User.class);
+		//vip virtual ip 虚拟ip  microservice-provider-user
+		ServiceInstance serviceInstance = loadBalancerClient.choose("microservice-provider-user");
+		System.out.println("11:"+serviceInstance.getServiceId()+"-"+serviceInstance.getHost()+"-"+serviceInstance.getPort());
+		return restTemplate.getForObject("http://microservice-provider-user/simple/"+id, User.class);
+		//return restTemplate.getForObject(userUrl+id, User.class);
+	}
+	
+	@GetMapping("/test")
+	public String test(){
+		ServiceInstance serviceInstance = loadBalancerClient.choose("microservice-provider-user");
+		System.out.println("11:"+serviceInstance.getServiceId()+"-"+serviceInstance.getHost()+"-"+serviceInstance.getPort());
+		
+		ServiceInstance serviceInstance2 = loadBalancerClient.choose("microservice-provider-user2");
+		System.out.println("22:"+serviceInstance2.getServiceId()+"-"+serviceInstance2.getHost()+"-"+serviceInstance2.getPort());
+		
+		return "1";
+		
+	}
+}
