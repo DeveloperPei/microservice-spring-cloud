1a1599a35da145f5497f71b4afcb5f54d81d2f2b
732846637@qq.com <732846637@qq.com>
2018-03-26 15:46:53 +0800

    eureka多实例运行（双Peer Aware Eureka服务器）
    通过运行多个实例并要求它们相互注册，容易忽视端口的配置

---
+++ microservice-discory-eureka-peer/src/main/java/com/itpx/cloud/EurekaPeerApplication.java
@@ -0,0 +1,13 @@
+package com.itpx.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaPeerApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaPeerApplication.class, args);
+	}
+}
